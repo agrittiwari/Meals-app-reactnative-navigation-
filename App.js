cbe3ca5af1753a9c9710269ff0b6b5b08d99f964
Agrit Tiwari <agrit1408@gmail.com>
2022-06-15 21:36:04 +0530

    writing boiler plate for screens

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Font from 'expo-font';
+import {AppLoading } from 'expo'
+
+const fetchFonts =() =>{
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold':require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
+
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ Screens/CategoriesScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native';
+
+const CategoryiesScreen = props=>{
+    return(
+
+        <View style={styles.screen}>
+            <Text> The Categories Screen</Text>
+        </View>
+    )
+};
+
+
+const styles = StyleSheet.create({
+    screen:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
+
+export default CategoryiesScreen;
+

50c4b94adbd019aa63042987212243b0e8c26942
Agrit Tiwari <agrit1408@gmail.com>
2022-06-18 19:26:44 +0530

    working on the details Screen

--- Screens/CategoryMealsScreen.js
+++ Screens/CategoryMealsScreen.js
@@ -12,7 +12,10 @@ const CategoriesMealsScreen = props=>{
         complexity={itemData.item.complexity}
         affordability={itemData.item.affordability}
         image={itemData.item.imageUrl}
-        onSelectMeal={()=>{} }/>
+        onSelectMeal={()=>{props.navigation.navigate({routeName:'MealDetail',
+    params:{
+        mealId: itemData.item.id
+    }})} }/>
     )
    }
     
--- Screens/MealDetailScreen.js
+++ Screens/MealDetailScreen.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import {View, Text, StyleSheet} from 'react-native';
+import { MEALS } from '../data/dummy-data';
+
 
 const MealDetailScreen = props=>{
+const mealId = props.navigation.getParam('mealId')
+const selectedMeal = MEALS.find(meal => meal.id === mealId)
     return(
 
         <View style={styles.screen}>
             <Text> The Meal Detail Screen</Text>
+            <Text>{selectedMeal.ingredients}</Text>
         </View>
     )
 };
+
+MealDetailScreen.navigationOptions = navigationData =>{
+    const mealId = navigationData.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId)
+return{
+    headerTitle: selectedMeal.title
+}
+}
 
 
 const styles = StyleSheet.create({
